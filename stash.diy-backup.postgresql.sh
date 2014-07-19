2f684a896f2f12b1a2dc1bb556eb8ac408bc5fc0
Matt Klich <matt.klich@readytalk.com>
2014-07-18 19:12:08 -0600

    Additional PostgreSQL backup options
    Also we now dynamically detect and use PosgteSQL 9.3+ features.

--- stash.diy-backup.postgresql.sh
+++ stash.diy-backup.postgresql.sh
@@ -4,13 +4,35 @@ check_command "pg_dump"
 check_command "psql"
 check_command "pg_restore"
 
+# Make use of PostgreSQL 9.3+ options if available
+psql_version="$(psql --version | awk '{print $3}')"
+if [[ ${psql_version//./} -ge 930 ]]; then
+    PG_PARALLELL="-j 5"
+    PG_SNAPSHOT_OPT="--no-synchronized-snapshots"
+fi
+
+# Use username if configured
+if [[ -n ${POSTGRES_USERNAME} ]]; then
+    PG_USER="-U ${POSTGRES_USERNAME}"
+fi
+
+# Use -h option if POSTGRES_HOST is set
+if [[ -n ${POSTGRES_HOST} ]]; then
+    PG_HOST="-h ${POSTGRES_HOST}"
+fi
+
+# Default port
+if [[ -z ${POSTGRES_PORT} ]]; then
+    POSTGRES_PORT=5432
+fi
+
 function stash_prepare_db {
     info "Prepared backup of DB ${STASH_DB} in ${STASH_BACKUP_DB}"
 }
 
 function stash_backup_db {
     rm -r ${STASH_BACKUP_DB}
-    pg_dump -Fd ${STASH_DB} -j 5 --no-synchronized-snapshots -f ${STASH_BACKUP_DB}
+    pg_dump ${PG_USER} ${PG_HOST} --port=${POSTGRES_PORT} ${PG_PARALLELL} -Fd ${STASH_DB} ${PG_SNAPSHOT_OPT} -f ${STASH_BACKUP_DB}
     if [ $? != 0 ]; then
         bail "Unable to backup ${STASH_DB} to ${STASH_BACKUP_DB}"
     fi
@@ -18,14 +40,14 @@ function stash_backup_db {
 }
 
 function stash_bail_if_db_exists {
-    psql -d ${STASH_DB} -c '' >/dev/null 2>&1
+    psql ${PG_USER} ${PG_HOST} --port=${POSTGRES_PORT} -d ${STASH_DB} -c '' >/dev/null 2>&1
     if [ $? = 0 ]; then
         bail "Cannot restore over existing database ${STASH_DB}. Try dropdb ${STASH_DB} first."
     fi
 }
 
 function stash_restore_db {
-    pg_restore -C -Fd -j 5 ${STASH_RESTORE_DB} | psql -q
+    pg_restore ${PG_USER} ${PG_HOST} --port=${POSTGRES_PORT} -C -Fd ${PG_PARALLEL} ${STASH_RESTORE_DB} | psql -q
     if [ $? != 0 ]; then
         bail "Unable to restore ${STASH_RESTORE_DB} to ${STASH_DB}"
     fi
