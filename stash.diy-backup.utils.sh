a773a61e96e36ec99a24bc4d04a1fd5946505aec
Nikolay Petrov <npetrov@atlassian.com>
2014-10-10 14:17:52 +1100

    DIY Backup HipChat integration

--- stash.diy-backup.utils.sh
+++ stash.diy-backup.utils.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function error {
-    echo "[${STASH_URL}] ERROR:" $*
+    echo "[${STASH_URL}] ERROR: $*"
+    hc_announce "[${STASH_URL}] ERROR: $*" "red"
 }
 
 function bail {
@@ -11,8 +12,14 @@ function bail {
 
 function info {
     if [ "${STASH_VERBOSE_BACKUP}" == "TRUE" ]; then
-        echo "[${STASH_URL}]  INFO:" $*
+        echo "[${STASH_URL}]  INFO: $*"
+        hc_announce "[${STASH_URL}]  INFO: $*" "gray"
     fi
+}
+
+function success {
+    echo "[${STASH_URL}]  SUCC: $*"
+    hc_announce "[${STASH_URL}]  SUCC: $*" "green"
 }
 
 function print {
@@ -23,4 +30,37 @@ function print {
 
 function check_command {
     type -P $1 &>/dev/null || bail "Unable to find $1, please install it and run this script again"
+}
+
+# $1 = message, $2 = color (yellow/green/red/purple/gray/random), $3 = notify (0/1)
+function hc_announce {
+    if [ -z "$1" ]; then
+        if [ "${STASH_VERBOSE_BACKUP}" == "TRUE" ]; then
+            echo "HipChat notification message is missing."
+        fi
+        return 1
+    fi
+    if [ -z "${HIPCHAT_ROOM}" ]; then
+        if [ "${STASH_VERBOSE_BACKUP}" == "TRUE" ]; then
+            echo "HipChat notification not send as the HIPCHAT_ROOM is not configured."
+        fi
+        return 1
+    fi
+    if [ -z "${HIPCHAT_TOKEN}" ]; then
+        if [ "${STASH_VERBOSE_BACKUP}" == "TRUE" ]; then
+            echo "HipChat notification not send as the HIPCHAT_TOKEN is not configured."
+        fi
+        return 1
+    fi
+
+    local COLOR="gray"
+    if [ -n "$2" ]; then
+        COLOR=$2
+    fi
+    local NOTIFY="false"
+    if [ "1" == "$3" ]; then
+        NOTIFY="true"
+    fi
+
+    curl -s -S -X POST -H "Content-Type: application/json" -d "{\"message\":\"$1\",\"color\":\"${COLOR}\",\"notify\":${NOTIFY}}" "${HIPCHAT_URL}/v2/room/${HIPCHAT_ROOM}/notification?auth_token=${HIPCHAT_TOKEN}"
 }
