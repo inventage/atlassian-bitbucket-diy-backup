3047d634e0fdb3f6e773c3cb9b25934a30b90924
Bradley Baetz <bradley.baetz@optus.com.au>
2015-05-19 15:32:14 +1000

    Run rsync in quiet mode when the backups are non-verbose

--- stash.diy-backup.rsync.sh
+++ stash.diy-backup.rsync.sh
@@ -7,8 +7,13 @@ function stash_perform_rsync {
       RSYNC_EXCLUDE_REPOS="${RSYNC_EXCLUDE_REPOS} --exclude=/shared/data/repositories/${repo_id}"
     done
 
+    RSYNC_QUIET=-q
+    if [ "${STASH_VERBOSE_BACKUP}" == "TRUE" ]; then
+        RSYNC_QUIET=
+    fi
+
     mkdir -p ${STASH_BACKUP_HOME}
-    rsync -avh --delete --delete-excluded --exclude=/caches/ --exclude=/data/db.* --exclude=/export/ --exclude=/log/ --exclude=/plugins/.*/ --exclude=/tmp --exclude=/.lock ${RSYNC_EXCLUDE_REPOS} ${STASH_HOME} ${STASH_BACKUP_HOME}
+    rsync -avh ${RSYNC_QUIET} --delete --delete-excluded --exclude=/caches/ --exclude=/data/db.* --exclude=/export/ --exclude=/log/ --exclude=/plugins/.*/ --exclude=/tmp --exclude=/.lock ${RSYNC_EXCLUDE_REPOS} ${STASH_HOME} ${STASH_BACKUP_HOME}
     if [ $? != 0 ]; then
         bail "Unable to rsync from ${STASH_HOME} to ${STASH_BACKUP_HOME}"
     fi
@@ -25,6 +30,11 @@ function stash_backup_home {
 }
 
 function stash_restore_home {
-    rsync -av ${STASH_RESTORE_HOME}/ ${STASH_HOME}/
+    RSYNC_QUIET=-q
+    if [ "${STASH_VERBOSE_BACKUP}" == "TRUE" ]; then
+        RSYNC_QUIET=
+    fi
+
+    rsync -av ${RSYNC_QUIET} ${STASH_RESTORE_HOME}/ ${STASH_HOME}/
     info "Performed restore of ${STASH_RESTORE_HOME} to ${STASH_HOME}"
 }
