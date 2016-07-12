e5bbe28d4a8172be289f520da1a4446cacd69a2a
Fraser Cobb <fcobb@atlassian.com>
2016-07-12 15:46:11 +1000

    BSERVDEV-13217: Move copy/share snapshot functions into aws-snapshots.sh

--- bitbucket.diy-backup.aws-snapshots.sh
+++ bitbucket.diy-backup.aws-snapshots.sh
@@ -97,5 +97,101 @@ function cleanup_old_offsite_rds_snapshots {
 }
 
 function cleanup_old_offsite_ebs_snapshots {
-    # Fraser implement me!
+    # TODO: Fraser implement me!
+    no_op
+}
+
+function share_and_copy_rds_snapshot {
+    local rds_snapshot_id="$1"
+    local source_aws_account_id=$(get_aws_account_id)
+
+    info "Waiting for RDS snapshot copy ${rds_snapshot_id} to become available before giving AWS account:${BACKUP_DEST_AWS_ACCOUNT_ID} permissions."
+    aws rds wait db-snapshot-completed --db-snapshot-identifier "${rds_snapshot_id}"
+
+    # Give permission to BACKUP_DEST_AWS_ACCOUNT_ID
+    aws rds modify-db-snapshot-attribute --db-snapshot-identifier "${rds_snapshot_id}" --attribute-name restore \
+        --values-to-add "${BACKUP_DEST_AWS_ACCOUNT_ID}" > /dev/null
+    info "Granted permissions on RDS snapshot ${rds_snapshot_id} for AWS account:${BACKUP_DEST_AWS_ACCOUNT_ID}"
+
+    # Assume BACKUP_DEST_AWS_ROLE
+    local creds=$(aws sts assume-role --role-arn ${BACKUP_DEST_AWS_ROLE} --role-session-name "BitbucketServerDIYBackup")
+    local aws_access_key_id="$(echo $creds | jq -r .Credentials.AccessKeyId)"
+    local aws_secret_access_key="$(echo $creds | jq -r .Credentials.SecretAccessKey)"
+    local aws_session_token="$(echo $creds | jq -r .Credentials.SessionToken)"
+
+    # Copy RDS snapshot to BACKUP_RDS_DEST_REGION in BACKUP_DEST_AWS_ACCOUNT_ID
+    local source_rds_snapshot_arn="arn:aws:rds:${AWS_REGION}:${source_aws_account_id}:snapshot:${rds_snapshot_id}"
+    AWS_ACCESS_KEY_ID=${aws_access_key_id} AWS_SECRET_ACCESS_KEY=${aws_secret_access_key} AWS_SESSION_TOKEN=${aws_session_token} \
+        aws rds copy-db-snapshot --region "${BACKUP_RDS_DEST_REGION}" --source-db-snapshot-identifier "${source_rds_snapshot_arn}" \
+            --target-db-snapshot-identifier "${rds_snapshot_id}" > /dev/null
+    info "Copied RDS Snapshot ${source_rds_snapshot_arn} as ${rds_snapshot_id} to ${BACKUP_RDS_DEST_REGION}"
+}
+
+function copy_rds_snapshot {
+    local source_rds_snapshot_id="$1"
+    local source_aws_account_id=$(get_aws_account_id)
+
+    info "Waiting for RDS snapshot ${source_rds_snapshot_id} to become available before copying to another region. This could take some time."
+    aws rds wait db-snapshot-completed --db-snapshot-identifier "${source_rds_snapshot_id}"
+
+    # Copy RDS snapshot to BACKUP_RDS_DEST_REGION
+    local source_rds_snapshot_arn="arn:aws:rds:${AWS_REGION}:${source_aws_account_id}:snapshot:${source_rds_snapshot_id}"
+    aws rds copy-db-snapshot --region "${BACKUP_RDS_DEST_REGION}" --source-db-snapshot-identifier "${source_rds_snapshot_arn}" \
+      --target-db-snapshot-identifier "${source_rds_snapshot_id}" > /dev/null
+    info "Copied RDS Snapshot ${source_rds_snapshot_arn} as ${source_rds_snapshot_id} to ${BACKUP_RDS_DEST_REGION}"
+}
+
+function copy_ebs_snapshot {
+    local source_ebs_snapshot_id="$1"
+    local source_region="$2"
+
+    info "Waiting for EBS snapshot ${source_ebs_snapshot_id} to become available in ${source_region} before copying to ${BACKUP_EBS_DEST_REGION}"
+    aws ec2 wait snapshot-completed --region ${source_region} --snapshot-ids ${source_ebs_snapshot_id}
+
+    # Copy snapshot to BACKUP_EBS_DEST_REGION
+    local dest_snapshot_id=$(aws ec2 copy-snapshot --region "${BACKUP_EBS_DEST_REGION}" --source-region "${source_region}" \
+         --source-snapshot-id "${source_ebs_snapshot_id}" | jq -r '.SnapshotId')
+    info "Copied EBS snapshot ${source_ebs_snapshot_id} from ${source_region} to ${BACKUP_EBS_DEST_REGION}. Snapshot copy ID: ${dest_snapshot_id}"
+
+    info "Waiting for EBS snapshot ${dest_snapshot_id} to become available in ${BACKUP_EBS_DEST_REGION} before tagging"
+    aws ec2 wait snapshot-completed --region ${BACKUP_EBS_DEST_REGION} --snapshot-ids ${dest_snapshot_id}
+
+    # Add tags to copied snapshot
+    aws ec2 create-tags --region ${BACKUP_EBS_DEST_REGION} --resources "${dest_snapshot_id}" --tags Key=Name,Value="${SNAPSHOT_TAG_VALUE}"
+    info "Tagged EBS snapshot ${dest_snapshot_id} with {Name: ${SNAPSHOT_TAG_VALUE}}"
+}
+
+function copy_and_share_ebs_snapshot {
+    local source_ebs_snapshot_id="$1"
+    local source_region="$2"
+
+    info "Waiting for EBS snapshot ${source_ebs_snapshot_id} to become available in ${source_region} before copying to ${BACKUP_EBS_DEST_REGION}"
+    aws ec2 wait snapshot-completed --region ${source_region} --snapshot-ids ${source_ebs_snapshot_id}
+
+    # Copy snapshot to BACKUP_EBS_DEST_REGION
+    local dest_snapshot_id=$(aws ec2 copy-snapshot --region "${BACKUP_EBS_DEST_REGION}" --source-region "${source_region}" \
+        --source-snapshot-id "${source_ebs_snapshot_id}" | jq -r '.SnapshotId')
+    info "Copied EBS snapshot ${source_ebs_snapshot_id} from ${source_region} to ${BACKUP_EBS_DEST_REGION}. Snapshot copy ID: ${dest_snapshot_id}"
+
+    info "Waiting for EBS snapshot ${dest_snapshot_id} to become available in ${BACKUP_EBS_DEST_REGION} before modifying permissions"
+    aws ec2 wait snapshot-completed --region ${BACKUP_EBS_DEST_REGION} --snapshot-ids ${dest_snapshot_id}
+
+    # Give BACKUP_DEST_AWS_ACCOUNT_ID permissions on the copied snapshot
+    aws ec2 modify-snapshot-attribute --snapshot-id "${dest_snapshot_id}" --region ${BACKUP_EBS_DEST_REGION} \
+        --attribute createVolumePermission --operation-type add --user-ids "${BACKUP_DEST_AWS_ACCOUNT_ID}"
+    info "Granted create volume permission on ${dest_snapshot_id} for account:${BACKUP_DEST_AWS_ACCOUNT_ID}"
+
+    info "Waiting for EBS snapshot ${dest_snapshot_id} to become available in ${BACKUP_EBS_DEST_REGION} before tagging"
+    aws ec2 wait snapshot-completed --region ${BACKUP_EBS_DEST_REGION} --snapshot-ids ${dest_snapshot_id}
+
+    info "Assuming AWS role ${BACKUP_DEST_AWS_ROLE} to tag copied snapshot"
+    local creds=$(aws sts assume-role --role-arn ${BACKUP_DEST_AWS_ROLE} --role-session-name "BitbucketServerDIYBackup")
+
+    # Add tag to copied snapshot, used to find EBS & RDS snapshot pairs for restoration
+    AWS_ACCESS_KEY_ID="$(echo $creds | jq -r .Credentials.AccessKeyId)" \
+        AWS_SECRET_ACCESS_KEY="$(echo $creds | jq -r .Credentials.SecretAccessKey)" \
+        AWS_SESSION_TOKEN="$(echo $creds | jq -r .Credentials.SessionToken)" \
+        aws ec2 create-tags --region ${BACKUP_EBS_DEST_REGION} --resources "${dest_snapshot_id}" \
+            --tags Key=Name,Value="${SNAPSHOT_TAG_VALUE}"
+    info "Tagged EBS snapshot ${dest_snapshot_id} with {Name: ${SNAPSHOT_TAG_VALUE}}"
 }
--- bitbucket.diy-backup.sh
+++ bitbucket.diy-backup.sh
@@ -4,10 +4,11 @@ SCRIPT_DIR=$(dirname $0)
 source ${SCRIPT_DIR}/bitbucket.diy-backup.utils.sh
 source ${SCRIPT_DIR}/bitbucket.diy-backup.common.sh
 
-if [ -f ${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh ]; then
-    source ${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh
+BACKUP_VARS_FILE="${BACKUP_VARS_FILE:-${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh}"
+if [ -f ${BACKUP_VARS_FILE} ]; then
+    source ${BACKUP_VARS_FILE}
 else
-    error "${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh not found"
+    error "${BACKUP_VARS_FILE} not found"
     bail "You should create it using ${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh.example as a template"
 fi
 
@@ -18,14 +19,14 @@ if [ -e "${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_HOME_TYPE}.sh" ]; then
     source ${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_HOME_TYPE}.sh
 else
     error "BACKUP_HOME_TYPE=${BACKUP_HOME_TYPE} is not implemented, ${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_HOME_TYPE}.sh does not exist"
-    bail "Please update BACKUP_HOME_TYPE in ${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh"
+    bail "Please update BACKUP_HOME_TYPE in ${BACKUP_VARS_FILE}"
 fi
 
 if [ -e "${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_DATABASE_TYPE}.sh" ]; then
     source ${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_DATABASE_TYPE}.sh
 else
     error "BACKUP_DATABASE_TYPE=${BACKUP_DATABASE_TYPE} is not implemented, ${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_DATABASE_TYPE}.sh does not exist"
-    bail "Please update BACKUP_DATABASE_TYPE in ${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh"
+    bail "Please update BACKUP_DATABASE_TYPE in ${BACKUP_VARS_FILE}"
 fi
 
 if [ -e "${SCRIPT_DIR}/bitbucket.diy-backup.${BACKUP_ARCHIVE_TYPE}.sh" ]; then
