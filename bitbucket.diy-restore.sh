55a035c72c0cd807fd7adcd85f856024b2352879
Fraser Cobb <fcobb@atlassian.com>
2016-07-22 11:59:36 +1000

    BSERVDEV-13371: Group restore/backup strategy sourcing into common.sh

--- bitbucket.diy-restore.sh
+++ bitbucket.diy-restore.sh
@@ -13,45 +13,13 @@
 set -e
 
 SCRIPT_DIR=$(dirname "$0")
-
 source "${SCRIPT_DIR}/utils.sh"
 source "${SCRIPT_DIR}/common.sh"
-
-BACKUP_VARS_FILE=${BACKUP_VARS_FILE:-"${SCRIPT_DIR}"/bitbucket.diy-backup.vars.sh}
-
-if [ -f "${BACKUP_VARS_FILE}" ]; then
-    source "${BACKUP_VARS_FILE}"
-    info "Using vars file: '${BACKUP_VARS_FILE}'"
-else
-    error "'${BACKUP_VARS_FILE}' not found"
-    bail "You should create it using '${SCRIPT_DIR}/bitbucket.diy-backup.vars.sh.example' as a template"
-fi
 
 # Ensure we know which user:group things should be owned as
 if [ -z "${BITBUCKET_UID}" -o -z "${BITBUCKET_GID}" ]; then
     error "Both BITBUCKET_UID and BITBUCKET_GID must be set in '${BACKUP_VARS_FILE}'"
     bail "See 'bitbucket.diy-backup.vars.sh.example' for the defaults."
-fi
-
-if [ -e "${SCRIPT_DIR}/home-${BACKUP_HOME_TYPE}.sh" ]; then
-    source "${SCRIPT_DIR}/home-${BACKUP_HOME_TYPE}.sh"
-else
-    error "BACKUP_HOME_TYPE=${BACKUP_HOME_TYPE} is not implemented, '${SCRIPT_DIR}/home-${BACKUP_HOME_TYPE}.sh' does not exist"
-    bail "Please update BACKUP_HOME_TYPE in '${BACKUP_VARS_FILE}'"
-fi
-
-if [ -e "${SCRIPT_DIR}/database-${BACKUP_DATABASE_TYPE}.sh" ]; then
-    source "${SCRIPT_DIR}/database-${BACKUP_DATABASE_TYPE}.sh"
-else
-    error "BACKUP_DATABASE_TYPE=${BACKUP_DATABASE_TYPE} is not implemented, '${SCRIPT_DIR}/database-${BACKUP_DATABASE_TYPE}.sh' does not exist"
-    bail "Please update BACKUP_DATABASE_TYPE in '${BACKUP_VARS_FILE}'"
-fi
-
-if [ -e "${SCRIPT_DIR}/archive-${BACKUP_ARCHIVE_TYPE}.sh" ]; then
-    source "${SCRIPT_DIR}/archive-${BACKUP_ARCHIVE_TYPE}.sh"
-else
-    error "BACKUP_ARCHIVE_TYPE=${BACKUP_ARCHIVE_TYPE} is not implemented, '${SCRIPT_DIR}/database-${BACKUP_ARCHIVE_TYPE}.sh' does not exist"
-    bail "Please update BACKUP_ARCHIVE_TYPE in '${BACKUP_VARS_FILE}'"
 fi
 
 ##########################################################
