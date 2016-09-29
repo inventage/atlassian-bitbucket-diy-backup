185743f6f0a80330803427fe2341fe77cf0fb51b
Charles Olivier <colivier@atlassian.com>
2016-09-29 17:04:30 +1000

    Removed cleanup of Elasticsearch snapshots

--- bitbucket.diy-backup.sh
+++ bitbucket.diy-backup.sh
@@ -72,7 +72,6 @@ success "Successfully completed the backup of your ${PRODUCT} instance"
 
 cleanup_db_backups
 cleanup_home_backups
-cleanup_elasticsearch_backups
 
 if [ -n "${BACKUP_ARCHIVE_TYPE}" ]; then
     info "Archiving backups and cleaning up old archives"
