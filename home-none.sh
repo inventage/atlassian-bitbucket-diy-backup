ec1a6850ed717291fca9eab3029cc0268a7cf331
Fraser Cobb <fcobb@atlassian.com>
2016-10-26 17:26:06 +1100

    BSERVDEV-14107: Add none strategy scripts

---
+++ home-none.sh
@@ -0,0 +1,36 @@
+# -------------------------------------------------------------------------------------
+# A backup and restore strategy that should only be used if you intentionally want to
+# backup or restore without including home directory information.
+# -------------------------------------------------------------------------------------
+
+function prepare_backup_home {
+    no_op
+}
+
+function backup_home {
+    no_op
+}
+
+function prepare_restore_home {
+    no_op
+}
+
+function restore_home {
+    no_op
+}
+
+function cleanup_home_backups {
+    no_op
+}
+
+function promote_home {
+    no_op
+}
+
+function replicate_home {
+    no_op
+}
+
+function setup_home_replication {
+    no_op
+}
